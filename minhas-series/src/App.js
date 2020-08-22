63bc5c2d9d2475f2b11c76cf98dd088b62a33415
Ljuniorcode <lucianojuniorarl@gmail.com>
2020-08-22 19:06:59 -0300

    03 - consumir api, generos, crud

--- minhas-series/src/App.js
+++ minhas-series/src/App.js
@@ -2,18 +2,22 @@ import React, { useState ,useEffect } from 'react'
 import Header from './Header'
 import Generos from './Generos'
 import axios from 'axios'
+import NovoGenero from './NovoGenero'
+import EditarGenero from './EditarGenero'
+
 import {
   BrowserRouter as Router,
   Route
 
 } from 'react-router-dom'
+// FIM DA IMPORTAÇÃO
+
+
+//INÍCIO DOS CÓDIGOS
 
 const Home = () => {
   return <h1>Home</h1>
 }
-
-
-
 
 function App() {
   const [data, setData] = useState({})
@@ -27,7 +31,10 @@ function App() {
       <div>
         <Header />
         <Route path='/' exact component={Home} />
-        <Route path='/generos' component={Generos} />
+        <Route path='/generos/:id' exact component={EditarGenero} />
+
+        <Route path='/generos/novogenero' exact component={NovoGenero} />
+        <Route path='/generos' exact component={Generos} />
         <pre>{JSON.stringify(data)}</pre>
       </div>
     </Router>
--- minhas-series/src/Generos.js
+++ minhas-series/src/Generos.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 const Generos = () => {
   const [data, setData] = useState([])
@@ -11,12 +12,22 @@ const Generos = () => {
       })
   }, [])
 
+  const deleteGenero = id => {
+    axios.delete('/api/genres/' + id)
+    .then(res => {
+      const filtrado = data.filter(item => item.id !== id)
+      setData(filtrado)
+    })
+  }
+
   const renderizaLinha = record => {
     return (
       <tr key={record.id}>
         <th scope='row'>{record.id}</th>
         <td>{record.name}</td>
-        <td><button>+</button></td>
+        <td><button className='btn btn-danger' onClick={() => deleteGenero(record.id)}>Remover</button>
+        <Link to={'/generos/' + record.id} className='btn btn-warning'>Editar</Link>        
+        </td>
       </tr>
     )
   }
@@ -34,7 +45,8 @@ const Generos = () => {
 
   return (
       <div className='container'>
-        <h1>Generos</h1>
+        <h1>Gêneros</h1>
+        <div><Link to='/generos/novogenero' className='btn btn-primary'>Novo Gênero</Link></div>
         <table className='table table-dark'>
           <thead>
             <tr>
